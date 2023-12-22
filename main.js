66991696bc775917906d4e749394656aad48e8dd
Xenko10 <kubinios_2002@o2.pl>
2023-12-22 11:21:28 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+
+const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 15;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+window.addEventListener("resize", onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
+}
+
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  controls.update();
+
+  render();
+
+  stats.update();
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+animate();
---
+++ readme.md
@@ -0,0 +1,9 @@
+## TO DO
+
+- [ ] medium size room
+- [ ] window with roller blind (you can click roller blind to show/hide sunlight)
+- [ ] desk with a book and a lamp (you can click on the lamp to turn it on/off)
+- [ ] frame with some photo
+- [ ] clock on the wall
+- [ ] door
+- [ ] wardrobe
