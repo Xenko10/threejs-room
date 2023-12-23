785d47529401ccc5b155426d7cc9fec13cac5a4d
Xenko10 <kubinios_2002@o2.pl>
2023-12-23 13:20:44 +0100

    add floor and walls, update readme

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module";
 
 const scene = new THREE.Scene();
-scene.add(new THREE.AxesHelper(5));
+// scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -11,17 +11,16 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-
-camera.position.z = 15;
+camera.position.x = 3;
+camera.position.y = 3;
+camera.position.z = 2;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-
 window.addEventListener("resize", onWindowResize, false);
-
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -31,6 +30,54 @@ function onWindowResize() {
 
 const stats = new Stats();
 document.body.appendChild(stats.dom);
+
+const loader = new THREE.TextureLoader();
+
+// meshes
+
+const floorTexture = loader.load("./img/floor.jpg");
+floorTexture.wrapS = THREE.RepeatWrapping;
+floorTexture.wrapT = THREE.RepeatWrapping;
+floorTexture.repeat.set(5, 3); // Adjust the repeat values as needed
+
+const floor = new THREE.Mesh(
+  new THREE.BoxGeometry(5.2, 4.2, 0.2),
+  new THREE.MeshPhongMaterial({
+    map: floorTexture,
+  })
+);
+floor.rotation.x = -Math.PI / 2;
+scene.add(floor);
+
+const walls = new THREE.Group();
+walls.add(
+  new THREE.Mesh(
+    new THREE.BoxGeometry(5.2, 2.5, 0.2),
+    new THREE.MeshPhongMaterial({
+      color: 0x808080,
+    })
+  ),
+  new THREE.Mesh(
+    new THREE.BoxGeometry(4.2, 2.5, 0.2),
+    new THREE.MeshPhongMaterial({
+      color: 0x808080,
+    })
+  )
+);
+walls.children[0].position.y = 1.35;
+walls.children[0].position.z = -2;
+walls.children[1].position.x = -2.5;
+walls.children[1].position.y = 1.35;
+walls.children[1].rotation.y = Math.PI / 2;
+scene.add(walls);
+
+// lights
+
+const ambientLight = new THREE.AmbientLight(0x404040, 10);
+
+scene.add(ambientLight);
+
+//  functions
 
 function animate() {
   requestAnimationFrame(animate);
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ## TO DO
 
-- [ ] medium size room
+- [x] medium size room
 - [ ] window with roller blind (you can click roller blind to show/hide sunlight)
 - [ ] desk with a book and a lamp (you can click on the lamp to turn it on/off)
 - [ ] frame with some photo
@@ -8,4 +8,7 @@
 - [ ] door
 - [ ] wardrobe
 - [ ] music instrument (you can click it for sound effects)
+- [ ] mirror
+- [ ] loading screen
+- [ ] laptop / computer setup
 - [ ] try to write code as clean as possible
