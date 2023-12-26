903016de261cc88a994ae69ee69e8e05811539a4
Xenko10 <kubinios_2002@o2.pl>
2023-12-26 17:28:48 +0100

    add object when it is loaded instead of adding all objects at the same time

--- main.js
+++ main.js
@@ -59,15 +59,18 @@ async function loadInterior() {
   const bed = await createBed();
   bed.position.set(-4.75, 0.08, 0.8);
   bed.rotation.y = Math.PI / 2;
+  scene.add(bed);
   const desk = await createDesk();
   desk.position.set(0.05, 0.1, -2);
   desk.rotation.y = Math.PI / 2;
+  scene.add(desk);
   const chair = await createChair();
   chair.position.set(-0.25, -0.075, -1.75);
   chair.rotation.y = Math.PI;
+  scene.add(chair);
   const wardrobe = await createWardrobe();
   wardrobe.position.set(2.2, 0.1, -2);
-  scene.add(bed, desk, chair, wardrobe);
+  scene.add(wardrobe);
 }
 
 loadInterior();
