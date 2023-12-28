3b32a4b53b5badd03d60a1e6ad2ff485c30353e7
Xenko10 <kubinios_2002@o2.pl>
2023-12-28 22:52:03 +0100

    delete light helpers

--- client/objects/lights.js
+++ client/objects/lights.js
@@ -7,17 +7,9 @@ export default function createLights() {
   pointLight.castShadow = true;
   pointLight.position.set(0, 2, 0);
 
-  // const pointLightHelper = new THREE.PointLightHelper(pointLight);
-  // lights.add(pointLightHelper);
-
   const directionalLight = new THREE.DirectionalLight(0xffffff, 0.1);
   directionalLight.castShadow = true;
   directionalLight.position.set(-0.5, 2, -6);
-
-  // const directionalLightHelper = new THREE.DirectionalLightHelper(
-  //   directionalLight
-  // );
-  // lights.add(directionalLightHelper);
 
   const ambientLight = new THREE.AmbientLight(0x404040, 2);
 
